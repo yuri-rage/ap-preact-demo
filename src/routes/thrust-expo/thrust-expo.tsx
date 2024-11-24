980f36812edfd47af645c804eec19065bd1f712d
Yuri Rage <yuri.rage@gmail.com>
2024-11-24 16:01:14 -0600

    ThrustExpo: add ultra-wide view

--- src/routes/thrust-expo/thrust-expo.tsx
+++ src/routes/thrust-expo/thrust-expo.tsx
@@ -22,6 +22,7 @@ import {
     thrustExpoPlotData,
     thrustExpoPlotLayout,
     onDataChange,
+    linearizedResult,
 } from './store';
 
 const DATA_COLUMNS = [
@@ -33,71 +34,88 @@ const DATA_COLUMNS = [
 
 export const ThrustExpo = () => {
     return (
-        <div className='max-w-screen-xl pb-8'>
-            <ToolTitle title='ArduPilot Thrust Expo'>
-                This tool estimates thrust linearization using thrust test stand data. Load a parameter file
-                or enter parameters manually. Copy and paste test stand data from a spreadsheet or enter it
-                manually. Current data is optional (for reference only - not used in calculation). Once the
-                plot is generated, adjust MOT_THST_EXPO to improve the linear fit. If the curve is poorly
-                matched at the extremes, do not chase a perfect fit. Rather, focus on midrange throttle
-                linearity.
-            </ToolTitle>
-            <div className='flex flex-col gap-3 px-3'>
-                <NumericGrid<ThrustData>
-                    title='Thrust Data'
-                    description='Paste test stand data or drag and drop a CSV or delimited text file.'
-                    columns={DATA_COLUMNS}
-                    rows={thrustDataRows}
-                    onDataChange={onDataChange}
-                />
-                <div className='flex w-full flex-row flex-wrap justify-between gap-3'>
-                    <ParamGrid
-                        paramConfig={paramConfig}
-                        params={params}
-                        isLoading={isParamGridLoading.value}
-                        description='Manually enter parameters or drag and drop a parameter file.'
-                        className='min-w-[550px] flex-1 basis-[550px]'
-                        variant='narrow'
+        <div className='flex min-w-[580px] flex-row flex-wrap gap-3 pb-8'>
+            <div className='w-full max-w-screen-xl'>
+                <ToolTitle title='ArduPilot Thrust Expo'>
+                    This tool estimates thrust linearization using thrust test stand data. Load a parameter
+                    file or enter parameters manually. Copy and paste test stand data from a spreadsheet or
+                    enter it manually. Current data is optional (for reference only - not used in
+                    calculation). Once the plot is generated, adjust MOT_THST_EXPO to improve the linear fit.
+                    If the curve is poorly matched at the extremes, do not chase a perfect fit. Rather, focus
+                    on midrange throttle linearity.
+                </ToolTitle>
+                <div className='flex flex-col gap-3 px-3'>
+                    <NumericGrid<ThrustData>
+                        title='Thrust Data'
+                        description='Paste test stand data or drag and drop a CSV or delimited text file.'
+                        columns={DATA_COLUMNS}
+                        rows={thrustDataRows}
+                        onDataChange={onDataChange}
                     />
-                    <div className='flex min-w-[550px] flex-1 basis-[550px] flex-col'>
-                        <div className='flex-none'>
-                            <ParamGrid
-                                title='Hover Thrust Estimation'
-                                paramConfig={hoverParamConfig}
-                                params={hoverParams}
-                                isLoading={isParamGridLoading.value}
-                                description='Optional: Enter number of motors and total mass to estimate hover thrust. Use MOT_HOVER_LEARN rather than setting the estimated value explicitly. The learned value may be useful to help validate the generated thrust curve.'
-                                variant='narrow'
-                                suppressToolbar={true}
-                            />
-                        </div>
-                        <div className='flex flex-1 flex-col justify-end'>
-                            <div className='flex justify-end gap-3 pt-3'>
-                                <Button variant='outline' onClick={showExampleData}>
-                                    Show Example
-                                </Button>
-                                <Button variant='outline' onClick={resetAll}>
-                                    Reset All
-                                </Button>
+                    <div className='flex w-full flex-row flex-wrap justify-between gap-3'>
+                        <ParamGrid
+                            paramConfig={paramConfig}
+                            params={params}
+                            isLoading={isParamGridLoading.value}
+                            description='Manually enter parameters or drag and drop a parameter file.'
+                            className='min-w-[550px] flex-1 basis-[550px]'
+                            variant='narrow'
+                        />
+                        <div className='flex min-w-[550px] flex-1 basis-[550px] flex-col'>
+                            <div className='flex-none'>
+                                <ParamGrid
+                                    title='Hover Thrust Estimation'
+                                    paramConfig={hoverParamConfig}
+                                    params={hoverParams}
+                                    isLoading={isParamGridLoading.value}
+                                    description='Optional: Enter number of motors and total mass to estimate hover thrust. Use MOT_HOVER_LEARN rather than setting the estimated value explicitly. The learned value may be useful to help validate the generated thrust curve.'
+                                    variant='narrow'
+                                    suppressToolbar={true}
+                                />
+                            </div>
+                            <div className='flex flex-1 flex-col'>
+                                {linearizedResult.value && (
+                                    <div className='text-sm pt-3 pl-2 pb-2'>
+                                        <div className='flex flex-col gap-1'>
+                                            <div className='font-semibold'>Calculated Values :</div>
+                                            <div className='flex gap-4 text-muted-foreground'>
+                                                <div>MOT_THST_EXPO: {params.value.MOT_THST_EXPO.toFixed(3)}</div>
+                                                <div>MOT_THST_HOVER: {hoverParams.value.MOT_THST_HOVER.toFixed(3)}</div>
+                                            </div>
+                                            <div className='text-muted-foreground'>StdDev: {linearizedResult.value.std_deviation.toFixed(3)}</div>
+                                        </div>
+                                    </div>
+                                )}
+                                <div className='flex flex-1 flex-col justify-end'>
+                                    <div className='flex gap-3'>
+                                        <Button variant='outline' onClick={showExampleData}>
+                                            Show Example
+                                        </Button>
+                                        <Button variant='outline' onClick={resetAll}>
+                                            Reset All
+                                        </Button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
+            </div>
+            <div className='flex w-full max-w-screen-xl flex-col gap-3'>
                 <PlotlyPlot
-                    className='h-[450px]'
+                    className='h-[450px] w-full'
                     data={thrustPlotData.value}
                     layout={thrustPlotLayout.value}
                 />
 
                 <PlotlyPlot
-                    className='h-[450px]'
+                    className='h-[450px] w-full'
                     data={thrustExpoPlotData.value}
                     layout={thrustExpoPlotLayout.value}
                 />
 
                 <PlotlyPlot
-                    className='h-[450px]'
+                    className='h-[450px] w-full'
                     data={thrustErrorPlotData.value}
                     layout={thrustErrorPlotLayout.value}
                 />
